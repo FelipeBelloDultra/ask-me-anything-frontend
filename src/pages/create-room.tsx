188c8d509d7fe0feb8c16fb84c5ebb594b4d3ad0
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-08-08 12:42:50 -0300

    chore: upgrade react version to 19

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,8 +1,15 @@
 import { ArrowRight } from "lucide-react";
 
 import amaLogo from "../assets/ama-logo.svg";
+import { useNavigate } from "react-router-dom";
 
 export function CreateRoom() {
+  const navigate = useNavigate();
+
+  function handleCreateRoom() {
+    navigate("/room/asdasd123123");
+  }
+
   return (
     <main className="h-screen flex items-center justify-center px-4">
       <div className="max-w-[450px] flex flex-col gap-6">
@@ -13,7 +20,10 @@ export function CreateRoom() {
           mais importantes para a comunidade.
         </p>
 
-        <form className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1">
+        <form
+          onSubmit={handleCreateRoom}
+          className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1"
+        >
           <input
             type="text"
             name="theme"
